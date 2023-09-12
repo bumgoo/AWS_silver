2b244485c24dfd2c49e1c40d41e7cb4be195b2eb
bumgoo <79138556+bumgoo@users.noreply.github.com>
2023-09-12 18:49:23 +0900

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DOCKER_APP_NAME=${컨테이너 명}
+
+cd /home/ec2-user/silver
+
+EXIST_CONTAINER=$(docker ps | grep $DOCKER_APP_NAME-main)
+
+if [ -z "$EXIST_CONTAINER" ]; then
+	docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml -d --build
+
+	echo "Build Complete ${DOCKER_APP_NAME}-main container"
+fi
+
+docker system prune -af
