8e58603e4b278719695ffebf8fc106eec7357707
Silver <programmerdlapp@gmail.com>
2023-09-13 09:26:06 +0900

    rds 연결

--- www/index.php
+++ www/index.php
@@ -1,3 +1,25 @@
 <?php
 echo 'hell ㅠㅠ';
+$db_host = "host=database-silver.c3al6husi4ee.ap-northeast-2.rds.amazonaws.com";
+$db_port = "port=5432";
+$db_user = "user=postgres";
+$db_password = "password=silver1234";
+$db_name = "dbname=postgres";
+
+$conn = pg_connect("$db_host $db_port $db_user $db_password $db_name");
+
+$query = "SELECT * FROM rds_table";
+$result = pg_query($conn, $query);
+
+$res = [];
+
+while ($row = pg_fetch_assoc($result)) {
+  $res[] = $row;
+}
+
+echo <<<EOM
+EC2 : Hello World<br/>
+RDS : $res[0]
+EOM;
+
 ?>
